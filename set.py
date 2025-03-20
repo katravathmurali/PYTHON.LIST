fee2d811f1a56199e3e4d9837872f73473337855
katravathmurali <muanikatravath@gmail.com>
2025-03-20 16:49:04 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,35 @@
+set1 = {'apple','banana','promaghanet'}
+print(set1)
+print(len(set1))
+print(type(set1))
+set1.add('kv')
+print(set1)
+set1.remove('kv')
+print(set1)
+set1.discard('banana')
+print(set1)
+set1.pop()
+print(set1)
+set1 = {88,22,33,44,55}
+set2 = {99,66,77,79,88}
+print(set1&set2)
+set1 = 'true'
+set2 = 'false'
+print(set1<set2)
+print(set1>set2)
+set1 = { 34,45,56,67,78}
+set2 = { 78,8,9,0,2,3,1}
+print(set1|set2)
+set1.update('oo','nn','mm')
+print(set1)
+set1.clear()
+print(set1)
+set1 = {"s","t","u","g"}
+set2 = {"k","l","p","b"}
+print(set1^set2)
+set = {5,6,7,8,1,2,3}
+Set = {1,2,3,4}
+print(set^Set)
+print(len(set))
+print(type(Set))
+
